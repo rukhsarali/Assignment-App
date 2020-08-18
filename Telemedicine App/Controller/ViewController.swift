27ba4bbc5de4b94f271cb2f51381630d335cd5b4
Rukhsarali <rukhsaralichanna7@gmail.com>
2020-08-18 17:12:44 +0500

    new update

--- Telemedicine App/Controller/ViewController.swift
+++ Telemedicine App/Controller/ViewController.swift
@@ -10,27 +10,23 @@ import UIKit
 
 class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
    
-    var arrayData = ["CustomerCare" , "Home" , "Menu option 1" , "Menu option 2" , "Menu option 3" , "Menu option 4" , "Menu option 5" , "Menu option 6" , "Logout"]
-    var arrayImage = [ #imageLiteral(resourceName: "customerCareImage") ,#imageLiteral(resourceName: "homeIcon") , #imageLiteral(resourceName: "menu1") , #imageLiteral(resourceName: "Menu2") , #imageLiteral(resourceName: "menu3") ,#imageLiteral(resourceName: "menu4") , #imageLiteral(resourceName: "menu5") , #imageLiteral(resourceName: "menu6") ,#imageLiteral(resourceName: "logout")  ]
     
-    @IBOutlet weak var sideView: UIView!  //side view full
     
-    @IBOutlet weak var profileView: UIView! // side view picture, name, email
-    
-    @IBOutlet weak var sideBar: UITableView! //side bar Tableview
-    
+    @IBOutlet weak var homeProfile: UIImageView!   //home picture
+    @IBOutlet weak var trailingView: NSLayoutConstraint!
+    @IBOutlet weak var leadingView: NSLayoutConstraint!
     @IBOutlet weak var profileImage: UIImageView! //profile picture
     
-    var isSideViewIsOpen : Bool = false
+    var arrayData = ["CustomerCare" , "Home" , "Menu option 1" , "Menu option 2" , "Menu option 3" , "Menu option 4" , "Menu option 5" , "Menu option 6" , "Logout"]
+    var arrayImage = [ #imageLiteral(resourceName: "customerCareImage") ,#imageLiteral(resourceName: "homeIcon") , #imageLiteral(resourceName: "menu1") , #imageLiteral(resourceName: "Menu2") , #imageLiteral(resourceName: "menu3") ,#imageLiteral(resourceName: "menu4") , #imageLiteral(resourceName: "menu5") , #imageLiteral(resourceName: "menu6") ,#imageLiteral(resourceName: "logout")  ]
+    var menuOut = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       roundPicture()
-        sideView.isHidden = true
-      //  sideView.backgroundColor = UIColor.white
-       isSideViewIsOpen = false
-       // profileView.isHidden = true
+
+        roundPicture(profileName: homeProfile)
+        roundPicture(profileName: profileImage)
+     
         
     }
 //MARK: - tableView Cell
@@ -53,76 +49,52 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
 
        tableView.deselectRow(at: indexPath, animated: true)
        
-       // tableView.tintColor = UIColor.black
+        let path = indexPath.row
+        
+        if path == 2 {
+            performSegue(withIdentifier: "menu1", sender: self)
+        }else if path == 3 {
+            performSegue(withIdentifier: "menu2", sender: self)
+        }
         
     }
   
     //MARK: - picture round function
     
-    func roundPicture(){
-        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
-        profileImage.clipsToBounds = true
-        profileImage.layer.borderColor = UIColor.white.cgColor
-        profileImage.layer.borderWidth = 3
+    func roundPicture(profileName: UIImageView){
+        
+        profileName.layer.cornerRadius = profileImage.frame.size.width / 2
+        profileName.clipsToBounds = true
+        profileName.layer.borderColor = UIColor.white.cgColor
+        profileName.layer.borderWidth = 3
+        
     }
     
 
     //MARK: - sideBar button
 
 
-   // @IBAction func sideBarButton(_ sender: UIButton) {
+  
         @IBAction func sideBarButton(_ sender: UIBarButtonItem) {
-            //profileView.isHidden = false
-                   sideBar.isHidden = false
-                   sideView.isHidden = false
-                   self.view.bringSubviewToFront(sideView)
-                  
-                   if !isSideViewIsOpen{
-                       isSideViewIsOpen = true
-                       sideView.frame = CGRect(x: 0, y: 88, width: 0, height: 808)
-                       sideBar.frame = CGRect(x: 1, y: 236, width: 0, height: 571)
-
-//                       UIView.setAnimationDuration(0.3)
-//                       UIView.setAnimationDelegate(self)
-//                       UIView.beginAnimations("TableAnimation", context: nil)
-
-                       sideView.frame = CGRect(x: 0, y: 88, width: 321, height: 808)
-                       sideBar.frame = CGRect(x: 1, y: 236, width: 319, height: 571)
-
-                       //UIView.commitAnimations()
-                    UIView.animate(withDuration: 0.9, delay: 0.0, options: .curveEaseIn, animations: {
-                        self.view.layoutIfNeeded()
-                    }) { (animationComplete) in
-                        print("animation completed")
-                    } //end
-                    
-                   }else {
-                       // profileView.isHidden = true
-                       sideBar.isHidden = true
-                       sideView.isHidden = true
-                       isSideViewIsOpen = false
-
-                       sideView.frame = CGRect(x: 0, y: 88, width: 321, height: 808)
-                       sideBar.frame = CGRect(x: 1, y: 236, width: 319, height: 571)
-
-//                       UIView.setAnimationDuration(0.5)
-//                       UIView.setAnimationDelegate(self)
-//                       UIView.beginAnimations("TableAnimation", context: nil)
-
-                       sideView.frame = CGRect(x: 0, y: 88, width: 0, height: 808)
-                       sideBar.frame = CGRect(x: 1, y: 236, width: 0, height: 571)
-                      // UIView.commitAnimations()
-                   
-           UIView.animate(withDuration: 0.9, delay: 0.0, options: .curveEaseIn, animations: {
-               self.view.layoutIfNeeded()
-           }) { (animationComplete) in
-               print("animation completed")
-           } //end
-            
-        }
+          
+            if menuOut == false {
+                leadingView.constant = 319
+                trailingView.constant = -319
+                menuOut = true
+            }else{
+                leadingView.constant = 0
+                trailingView.constant = 0
+                menuOut = false
+            }
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveEaseIn, animations: {
+                self.view.layoutIfNeeded()
+            }) { (animationComplete) in
+                print("animation completed")
+            } //animation end
             
             
-        }
+            
+        } // button end
         
         
         
@@ -131,7 +103,5 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
     }
     
     
-//}
-
 
 
